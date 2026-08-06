d908235f9f8a1247a22e5eaf54a2a3e7ac94aa1c
gaellebega <gaellebega@gmail.com>
2026-08-06 14:55:02 +0200

    the variable declaration in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Header from "./components/Header";
 import TaskList from "./components/TaskList";
-
+// the way we define the dat type of a variable in typescript
+//this is the normal way in jslet name="Piyush";
+let name: string ="Piyush"
 function App() {
   return (
     <div className="min-h-screen bg-blue-100 flex justify-center items-center">
